2ef7b635c060740e55d047a2d96911d78f3c1091
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:25:27 +0100

    Initial commit

--- src/versions.tf
+++ src/versions.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  required_providers {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = ">= 0.25"
+    }
+  }
 }
